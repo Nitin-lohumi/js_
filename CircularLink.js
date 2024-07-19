07687c0bf3eb35f66b245908a50761d737a6f1c5
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-07-19 11:21:17 +0530

    Add

---
+++ CircularLink.js
@@ -0,0 +1,77 @@
+class Newnode{
+    constructor(data){
+      this.data  = data;
+      this.next =null;
+    }
+}
+class circularLinkList{
+    constructor(){
+        this.head =null;
+        this.size = 0;
+    }
+    insert(element){
+        let newnode =new  Newnode(element);
+        let temp;
+        if(this.head ==null){
+            this.head = newnode;
+        }
+        else{
+            temp = this.head;
+            while(temp.next){
+                temp=temp.next;
+            }
+            temp.next= newnode;
+            // newnode.next = this.head;
+        }
+        console.log("inserted");
+        this.size++;
+    }
+    delete(index){
+        let temp;
+        let prev;
+        let  i=0;
+        if(this.head ===null){
+            console.log("empty linked list ");
+            return;
+        }
+        else if(index==0){
+             this.head = this.head.next;
+             temp = this.head;
+             while(this.temp!=this.head){
+                temp = temp.next;
+             }
+             temp.next = this.head;
+        }
+        else{
+            while(i<index){
+                i++;
+                temp = this.head;
+                prev = temp;
+                temp = temp.next;
+            }
+            prev.next = temp.next;
+        }
+        // temp.next = this.head;
+        this.size--;
+        console.log("deleted");
+    }
+    print(){
+        let temp; 
+        temp = this.head;
+        if(this.head==null){
+            console.log("this is null");
+        }
+        else{
+            while(temp){
+                console.log(temp.data);
+                temp = temp.next;
+            }
+        }
+    }
+}
+
+let cl = new circularLinkList();
+cl.insert(10);
+cl.insert(20);
+cl.delete(1);
+cl.print();
