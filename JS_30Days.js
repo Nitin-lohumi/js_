a4295d9335a1ae70b239eeef3a99335ac52e398e
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-05 13:55:40 +0530

    add

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,9 +1,29 @@
 
-var argumentsLength = function(...args) {
-    //  while(arguments<args.length){
-    //     arguments++;
-    //  }
-    //  return arguments;
-    return args.length;
-};
-console.log(argumentsLength("3"));
+
+function memoize(fn) {
+    let cache={};
+    return function(...args) {
+        const key  = args;
+        console.log(key);
+          if(key in cache){
+            return cache[key];
+          }
+          cache[key] = fn(...args);
+          console.log(cache);
+          return cache[key];
+
+        }
+
+}
+
+
+let callCount = 0;
+ const memoizedFn = memoize(function (a, b) {
+    	 callCount += 1;
+       return a + b;
+
+ })
+console.log(memoizedFn(2, 2)); // 5
+console.log(memoizedFn(1, 2)); //5 
+ console.log(callCount);
+
