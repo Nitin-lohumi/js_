a1ccc71db0871ba72510eea6fe82afdc1e8438d7
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-08 23:04:10 +0530

    Add

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,72 +1,17 @@
-class Node{
-  constructor(data){
-    this.data = data;
-    this.next = null;
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+  let excution;
+  return function(...args) { 
+      clearTimeout(excution);
+      excution=setTimeout(()=>fn(...args),t);
   }
-}
-class Linkedlist {
-  constructor() {
-     this.head = null;
-  }
-  createLinklist(data){
-    let newnode = new Node(data);
-    return newnode;
-  }
-  add(data){
-    if(this.head==null){
-      this.head = this.createLinklist(data);
-    }
-    else{
-      let current = this.head;
-       let newnode = new Node(data);
-       while(current.next!=null){
-        current = current.next;
-       }
-       current.next = newnode;   
-      }
-  }
-  removeFromlast(){
-    if(this.head==null){
-      console.log("linked list is underflow");
-      return; 
-    }
-    else{
-      let current = this.head;
-      let temp;
-      while(current.next!=null){
-         temp = current;
-         current = current.next;
-      }
-      temp.next = null;
-    }
-  }
-  display(){
-    let current = this.head;
-    while(current!=null){
-      console.log(current.data);
-      current=current.next;
-    }
-  }
-  isPallindrom(){
-    let current = this.head;
-    let string ="";
-    while(current!=null){
-       string += current.data;
-      current = current.next;
-    }
-    const strRev =  [...string].reverse().join("");
-    if(string.match(strRev)){
-      return true;
-    }else{
-      return false;
-    }
-  }
-}
-let ll = new Linkedlist();
-ll.add(1)
-ll.add(2)
-ll.add(2);
-ll.add(100);
-console.log(ll.isPallindrom());
-ll.display();
+};
+ const log = debounce(console.log, 1000);
+  console.log(log(1));
+  console.log(log(2));
+
 
