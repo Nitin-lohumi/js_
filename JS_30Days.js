cb16a1701d74f7d61e162d6fda05e7a3dd19b12b
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-10 22:39:31 +0530

    js circular linked list

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,51 +1,10 @@
-class Node {
-  constructor(element){
-    this.data = element;
-    this.next = null;
-  }
-}
-class CricularQueue {
-  constructor(){
-    this.front = null;
-    this.rare = null;
-  }
-  insert(element){
-    let newnode  = new Node(element);
-    if(this.front==null||this.rare==null){
-     this.front = this.rare = newnode;
+var chunk = function(arr, size) {
+  let arrchunk = [];
+    for(let i=0; i<arr.length; i = i+size){
+     arrchunk.push(arr.slice(i,i+size));
     }
-    else{
-      this.rare.next = newnode;
-      this.rare = newnode;
-      this.rare.next = this.front;
-    }
-  }
-  remove(){
-    if(this.front==null||this.rare==null){
-      console.log(null);
-     }
-     else{
-      this.front= this.front.next;
-      this.rare.next = this.front;    
-     }
-  }
-  display(){
-    var temp=this.front
-    do
-    {
-      console.log(temp.data)
-      temp=temp.next
-    }while(temp!=this.front)
-  }
-}
-
-let n = new CricularQueue();
-n.insert(10);
-n.insert(20);
-n.insert(30);
-n.insert(40);
-n.insert(50);
-n.insert(60);
-n.insert(70);
-n.remove();
-n.display();
+    return arrchunk;
+};
+let arr =[1,2,3,4,5];
+let size =2;
+chunk(arr,size);
