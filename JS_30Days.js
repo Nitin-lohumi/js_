a895ea1851735efef7c4b64a6f4d4492254ad76f
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-12 12:23:02 +0530

    add

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,10 +1,42 @@
-Array.prototype.last = function() {
-   if(!this.length) return -1;
-    for(let i=0; i<this.length;i++){
-       return this[this.length-1];
+class RandomizedSet {
+   constructor() {
+      this.arr =[];
+   
+   }
+   insert(val) {
+    for(let i=0;i<this.arr.length; i++){
+      if(this.arr[i]===val){
+         return false;
+      }
     }
-};
+     this.arr.push(val);
+     this.map.set(val,this.arr.length-1);
+     return true;
+   }
+   remove(val) {
+      if(this.arr.length==0){
+         return false;
+      }
+      for(let i=0;i<=this.arr.length; i++){
+         if (this.arr[i]==val) {
+            this.arr.splice(i, 1);
+            this.map.delete(val);
+            return true;
+          }
+        }
+     return false;
+   }
 
-  const arr = [null,{},3];
-  console.log(arr.last()); // 3
- 
+   getRandom = function() {
+      let randomnumber = this.arr[Math.floor(Math.random() * this.arr.length)];
+      return randomnumber;
+   };
+}
+let random = new RandomizedSet();
+console.log(random.arr);
+console.log(random.insert(3));
+console.log(random.insert(120));
+console.log(random.remove(3));
+console.log(random.map);
+
+
