33001cf2c598f5c96ee43788c0180336ded7cef0
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-21 14:00:21 +0530

    add

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,17 +1,15 @@
-var flat = function (arr) {
-     let newarray = [];
-    //  if(n==0){
-    //     return arr;
-    //  }
-     for(let i=0; i<arr.length; i++){
-         if(typeof(arr[i])=='object'){
-           newarray.push(...flat(arr[i]));
-         }else{
-            newarray.push(arr[i]);
-         }
-     }
-     return newarray;
+var lengthOfLongestSubstring = function(s) {
+  let count = 0;
+  let maxSize =0;
+  let set  = new Set();
+  for (let i = 0; i < s.length; i++){
+    while(set.has(s[i])){
+       set.delete(s[count]);
+       count++;
+    }
+    set.add(s[i]);
+    maxSize = Math.max(maxSize,i-count+1);
+  }
+  return maxSize;
 };
-arr = [1,2,3,4,5,[4,5,67,7,88,9],[1,2,3,5,6,7,[8,2,3,45,6,7],345,6,7,8,9]];
-
-console.log(flat(arr));
+console.log(lengthOfLongestSubstring("abcabcbb"));
