3146db4da49be3d304fbfd8335c7689c6674f887
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-08-28 15:01:11 +0530

    Add program

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,16 +1,5 @@
-
-var candy = function(rate) {
-  let candy = new Array(rate.length).fill(1);
-  for (let i = 1; i < rate.length; i++) {
-   if(rate[i]>rate[i-1]){
-     candy[i] = candy[i-1]+1;
-   }     
-  }
-  for(let i =rate.length-2; i>=0; i--){
-   if(rate[i]>rate[i+1]){
-     candy[i] = Math.max(candy[i],candy[i+1]);
-   }
-  }
-  return candy.reduce((i,j)=>i+j,0);
-};
-console.log(candy([1,2,2]));
+var isPalindrome = function(s) {
+  let arr = s.split('').filter((v)=>(v!==":")&&(v!=",")&&(v!=" ")&&(v!="~")&&(v!="!")&&(v!="@")&&(v!="#")&&(v!="$")&&(v!="%")&&(v!="^")&&(v!="&")&&(v!="*")&&(v!="(")&&(v!=")")&&(v!="-")&&(v!="+")&&(v!="=")&&(v!="|")&&(v!="}")&&(v!="{")&&(v!=";")&&(v!="]")&&(v!="[")&&(v!='"')&&(v!=";")&&(v!="'")&&(v!=">")&&(v!="<")&&(v!=".")&&(v!="?")&&(v!="/")&&(v!="_")&&(v!="`")&&(v!="\\"));
+  return arr.join("").toString().toLowerCase().match(arr.reverse().join("").toString().toLowerCase())?true:false;
+  };
+console.log(isPalindrome("ab_a"));
