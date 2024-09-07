3e44dcbeac2fc210e3f15489e37497c75ff0069f
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-09-07 19:08:25 +0530

    add

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,21 +1,10 @@
-var canConstruct = function(ransomNote, magazine) {
-  let map = {};
-  let i= 0;
-  let count =0;
-  if(ransomNote.length>magazine.length){
-    return false;
-  }
-  while(i<magazine.length){
-    map[ransomNote[i]] = i;
-    console.log(map);
-    if(map.hasOwnProperty(magazine[i])){
-      count ++;
-      delete map[ransomNote[i]];
+var canConstruct = function (ransomNote, magazine) {
+  for (const char of magazine) 
+    {
+        ransomNote = ransomNote.replace(char,"");
+        console.log(ransomNote)
     }
-    console.log(" after map ",map)
-     i++;
-  }
-  console.log(count);
-  return count === ransomNote.length;
-};let ransomNote = "aab", magazine = "baa";
-console.log(canConstruct(ransomNote,magazine));
+    return !ransomNote?true:false;
+};
+let ransomNote = "aab", magazine = "baa";
+console.log(canConstruct(ransomNote, magazine));
