f584cfae61e0cde1fb5407ffa1590a0ad91f73dd
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-09-14 12:00:22 +0530

    add

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,9 +1,17 @@
-var canConstruct = function (ransomNote, magazine) {
-  for (const char of magazine) {
-    ransomNote = ransomNote.replace(char, "");
+var isIsomorphic = function (s, t) {
+  let map1 = [];
+  let map2 = [];
+  if(s.length!==t.length) return false
+  for (let c = 0; c < s.length; c++) {
+   if(map1[s.charAt(c)]!=map2[t.charAt(c)]){
+    console.log(map1)
+    console.log(map2)
+    return false;
+   }
+   map1[s.charAt(c)] = c+1;
+   map2[t.charAt(c)] = c+1;
   }
-  return !ransomNote ? true : false;
+   return true
 };
-let ransomNote = "aab",
-  magazine = "baa";
-console.log(canConstruct(ransomNote, magazine));
+let s = "add", t = "eag";
+console.log(isIsomorphic(s, t));
