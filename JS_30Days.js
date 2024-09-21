238631132ff4caa57ca3e19377b0657fd037d786
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-09-21 11:17:25 +0530

    add

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,20 +1,57 @@
-var isHappy = function(n) {
-  if(n==1||n==7){
-    return true;
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
   }
-  while(n>9){
-    let arr = n.toString().split("").map(Number);
-    let pow = arr.map((num)=>{
-      return Math.pow(num,2);
-    })
-    n = pow.reduce((pre,curr)=>{
-      return pre+curr;
-    }, 0);
-    if(n==1||n==7){
-      return true;
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  insert(data) {
+    let newnode = new Node(data);
+    if (this.head === null) {
+      this.head = newnode;
+      return;
+    }
+    let temp = this.head;
+    while (temp.next) {
+      temp = temp.next;
+    }
+    temp.next = newnode;
+  }
+  printList() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
     }
   }
-  return false;
-};
-let n = 2;
-console.log(isHappy(n));
+  delete(data){
+    if(this.head.data==data){
+        this.head =  this.head.next;
+        return;
+    }else{
+        let temp = this.head;
+        while(temp.next){
+            if(data===temp.next.data){
+             temp.next = temp.next.next;
+             return;
+            }
+            temp = temp.next;
+        }
+    }
+  }
+}
+
+let ll = new LinkedList();
+ll.insert(10);
+ll.insert(20);
+ll.insert(30);
+ll.insert(40);
+ll.insert(50);
+ll.insert(60);
+
+ll.delete(60);
+
+ll.printList();
