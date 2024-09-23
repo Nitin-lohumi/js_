65a89949e6a5a23fd57edabbd37fe6559c726051
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-09-23 09:58:15 +0530

    add

--- Try.js
+++ Try.js
@@ -1,63 +1,74 @@
 class Node {
-  constructor(val) {
-    this.val = val;
+  constructor(data){
+    this.data = data;
     this.left = null;
     this.right = null;
   }
 }
-class Bt {
+class BinaryTree{
   constructor() {
     this.root = null;
   }
-  insert(data) {
-    let node = new Node(data);
-    if (this.root === null) {
-      this.root = node;
-    } else {
-      const search = (node) => {
-        if (data < node.val && node.left) {
-          search(node.left);
-        } else if (data < node.val) {
-          node.left = new Node(data);
-        } else if (data > node.val && node.right) {
-          search(node.right);
-        } else if (data > node.val) {
-          node.right = new Node(data);
-        }
-      };
-      return search(node);
+  insert(data){
+    const newnode = new Node(data);
+    if(this.root===null){
+      this.root = newnode;
+    }else{
+      this.insert_node(this.root,newnode);
     }
   }
-  find(data) {
-    if (this.root == null) {
-      return false;
-    }
-    let node = this.root;
-    let found = false;
-    while (node && !found) {
-      if (data > node.val) {
-        node = node.right;
-      } else if (data < node.val) {
-        node = node.left;
-      } else {
-        found = node;
+  insert_node(node,newnode){
+    if(newnode.data<node.data){
+      if(node.left==null){
+        node.left = newnode;
+      }else{
+        this.insert_node(node.left,newnode);
       }
     }
-    return found ? true : false;
+    else{
+      if(node.right==null){
+        node.right = newnode;
+      }else{
+        this.insert_node(node.right,newnode);
+      }
+    }
   }
-  inorder(node) {
+  inorder(node = this.root) {
+    let arr = [];
+     function inorderfun(root){
+      if(!root){
+        return;
+      }
+      inorderfun(root.left);
+      arr.push(root.data);
+      inorderfun(root.right);
+     }
+     inorderfun(node)
+     console.log(arr)
+  }
+
+  preorder(node = this.root) {
     if (node !== null) {
-      this.inorder(node.left);
-      console.log(node.val);
-      this.inorder(node.right);
+      console.log(node.data);
+      this.preorder(node.left);
+      this.preorder(node.right);
+    }
+  }
+  postorder(node = this.root) {
+    if (node !== null) {
+      this.postorder(node.left);
+      this.postorder(node.right);
+      console.log(node.data);
     }
   }
 }
-let tree = new Bt();
-tree.insert(10);
-tree.insert(20);
-tree.insert(30);
+let tree = new BinaryTree();
 tree.insert(40);
-tree.insert(50);
-tree.insert(1);
-tree.inorder(tree.root);
+tree.insert(20);
+tree.insert(60);
+console.log("this is Inorder");
+tree.inorder();
+console.log("this is preorder");
+tree.preorder();
+console.log("this is postoreder");
+tree.postorder()
