ea9b1fac6bff6f0b8c82e88e621bcc86fd5853aa
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-09-27 12:27:58 +0530

    Add

--- AVL.js
+++ AVL.js
@@ -53,6 +53,13 @@ class AvlTree{
     y.height = 1+ Math.max(this.height(y.left),this.height(y.right));
     return y;
    }
+   getMini(node){
+    let cur = node;
+    while(cur.left!==null){
+        cur = cur.left;
+    }
+    return cur;
+   }
 
    insert(node, value) {
     if (node === null) {
@@ -91,6 +98,54 @@ class AvlTree{
    insertValue(data){
     this.root = this.insert(this.root,data);
    }
+
+   deleteNode(node,data){
+    if(node===null) return node;
+   
+    if(data<node.data){
+        node.left =this.deleteNode(node.left , data);
+    }
+    else if(data>node.data){
+        node.right = this.deleteNode(node.right,data);
+    }
+    else {
+        if(node.left ==null){
+            return node.right;
+        }else if(node.right==null){
+            return node.left;
+        }
+
+        const temp = this.getMini(node.right);
+        node.data = temp.data;
+        node.right = this.deleteNode(node.right,temp.data);
+    }
+    node.height = 1 + Math.max(this.height(node.left),this.height(node.right));
+
+    const balance = this.balancingFactor(node);
+
+
+    if(balance>1&& this.balancingFactor(node.left)>=0){
+        return this.rightRoation(node);
+    }
+    if(balance>1 && this.balancingFactor(node.left)<0){
+        node.left = this.leftRotaion(node);
+        return this.rightRoation(node);
+    }
+    if(balance<-1&& this.balancingFactor(node,right)<=0){
+         return this.rightRoation(node);
+    }
+    if(balance<-1 && this.balancingFactor(node.right)<0){
+        node.right = this.rightRoation(node);
+        return this.leftRotaion(node);
+    }
+    return node;
+   }
+   
+   deleteValue(value) {
+    this.root = this.deleteNode(this.root, value);
+  }
+
+
    inOrder(node) {
     if (node !== null) {
       this.inOrder(node.left);
@@ -112,6 +167,10 @@ tree.insertValue(40);
 tree.insertValue(50);
 tree.insertValue(25);
 
-// tree.display();
+tree.display();
 
-console.log(tree.root)
+
+tree.deleteValue(20);
+
+console.log("hiiiiiiiiiiiiiiiiii")
+tree.display();
