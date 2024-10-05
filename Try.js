70b560d89eee58a53db4e581a4db1f6bbc412f8d
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-10-05 17:48:02 +0530

    add

--- Try.js
+++ Try.js
@@ -1,13 +1,14 @@
-var removeNthFromEnd = function(head, n) {
-   let res = new ListNode(0, head);    
-   let newHead = res;
-   for(let i=0; i<n; i++){
-    head = head.next;
-   }
-   while(head!=null){
-     head = head.next;
-     newHead =  newHead.next;
-   }
-   newHead.next = newHead.next.next;
-   return res.next;
+var reverse = function(x) {
+const MAX_INT = 2**31-1
+const MIN_INT = -(2**31);
+let reverse =0;
+const sign = x<0?-1:1;
+x = Math.abs(x)
+reverse = parseInt(x.toString().split('').reverse().join(''));
+reverse =reverse*sign;
+if(reverse<MIN_INT||reverse>MAX_INT){
+  return 0;
+}
+return reverse;
 };
+console.log(reverse(1534236469))
