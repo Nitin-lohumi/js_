a725b9d252b56ed1935d8d1b5a64e1f7ad6c2f3c
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-10-20 22:39:15 +0530

    addd

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,22 +1,11 @@
-var wordPattern = function(pattern, s) {
-    let map = new Map()
-    let map1 = new Map()
-    let arr =s.split(' ');
-    for (let i = 0; i < pattern.length; ++i) {
-      if (map.has(pattern[i]) && arr[i] !== map.get(pattern[i])) {
-          return false;
-      } else {
-          map.set(pattern[i],arr[i]);
-      }
-  }  
-  for (let i = 0; i < arr.length; ++i) {
-    if (map1.has(arr[i]) && pattern[i] !== map1.get(arr[i])) {
-        return false;
-    } else {
-        map1.set(arr[i],pattern[i]);
+var singleNumber = function(nums) {
+    let a= nums.sort((a,b)=>a-b);
+ 
+     for(let i=0;i<a.length;i+=2){
+         if(a[i]!==a[i+1])
+            return a[i];   
     }
-}
-   return true
-};    
-let pattern = "abba", s ="dog dog dog dog"
-console.log(wordPattern(pattern,s));
+ }
+console.log(singleNumber([4,1,2,1,2]));
+
+//1,1,2,2,4
