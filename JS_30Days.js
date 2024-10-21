9c86625bbe281974d108d44d713347b1a76218a3
NITIN LOHUMI <lohuminitin082@gmail.com>
2024-10-21 14:24:56 +0530

    ad

--- JS_30Days.js
+++ JS_30Days.js
@@ -1,11 +1,60 @@
-var singleNumber = function(nums) {
-    let a= nums.sort((a,b)=>a-b);
- 
-     for(let i=0;i<a.length;i+=2){
-         if(a[i]!==a[i+1])
-            return a[i];   
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  add(element) {
+    let node = new Node(element);
+    let current;
+    if (this.head == null) this.head = node;
+    else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
     }
- }
-console.log(singleNumber([4,1,2,1,2]));
+    this.size++;
+  }
 
-//1,1,2,2,4
+  deleteDuplicates = function (head) {
+    if (head == null) {
+      return head;
+    } else {
+      let temp = head.next;
+      let prev = head;
+      while (temp != null) {
+        if (prev.element === temp.element) {
+          prev.next = temp.next;
+        } else {
+          prev = temp;
+        }
+        temp = temp.next;
+      }
+      return head;
+    }
+  };
+  printList() {
+    let curr = this.head;
+    let str = "";
+    while (curr) {
+      str += curr.element + " ";
+      curr = curr.next;
+    }
+    console.log(str);
+  }
+}
+const list = new LinkedList();
+list.add(1);
+list.add(1);
+list.add(1);
+// list.add(3);
+// list.add(3);
+console.log(list.deleteDuplicates(list.head));
+list.printList();
