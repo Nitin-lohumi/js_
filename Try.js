43bb4b291cf3d70542145bddd02ee042f2129f7e
Nitin-Lohumi <lohuminitin@gmail.com>
2025-01-10 12:46:16 +0530

    Add

--- Try.js
+++ Try.js
@@ -1,16 +1,37 @@
-var countKDifference = function(nums, k) {
-  const obj ={};
-  let count =0;
-  for(let n of nums){
-    if(obj[n-k]){
-      count=count+obj[n-k];
-    }
-    if(obj[n+k]){
-      count=count+obj[n+k];
-    }
-    obj[n]= ((obj[n]||0)+1);
-    console.log(obj)
-  }
-   return count;
-};
-console.log(countKDifference([1,2,2,1],1))//2-1=1
+var wordSubsets = function(words1, words2) {
+   const targetFreq = new Array(26).fill(0);
+   for (const word of words2) {
+     const freq = new Array(26).fill(0);
+
+     for (const char of word) {
+       freq[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+     }
+     
+     for (let i = 0; i < 26; i++) {
+       targetFreq[i] = Math.max(targetFreq[i], freq[i]);
+     }
+   }
+   const result = [];
+   for (const word of words1) {
+     const freq = new Array(26).fill(0);
+     for (const char of word) {
+       freq[char.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+     }
+     let isUniversal = true;
+     for (let i = 0; i < 26; i++) {
+       if (freq[i] < targetFreq[i]) {
+         isUniversal = false;
+         break;
+       }
+     }
+     if (isUniversal) {
+       result.push(word);
+     }
+   }
+ 
+   return result;
+ };
+ 
+ console.log(
+   wordSubsets(["amazon", "apple", "facebook", "google", "leetcode"], ["e", "o"])
+ );
