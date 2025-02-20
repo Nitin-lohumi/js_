b081f034bd64224f2c646eb46d49a699c1c9e750
Nitin-Lohumi <lohuminitin@gmail.com>
2025-02-20 15:56:13 +0530

    Add

--- Try.js
+++ Try.js
@@ -1,11 +1,30 @@
-var isPowerOfFour = function (n) {
-  if (n == 1) {
-    return true;
-  }
-  if(n==0){
+var searchMatrix = function (matrix, target) {
+  let arr = matrix.flat();
+  function binarysearch(arr, target,left,right) {
+    if(left>right){
+      return false;
+    }
+    let mid =Math.floor(left + (right - left) / 2);
+    if(arr[mid]===target){
+      return true;
+    }
+    else if(arr[mid]<target){
+      return binarysearch(arr,target,mid+1,right);
+    }
+    else if(arr[mid]>target){
+      return binarysearch(arr,target,left,mid-1);
+    }
     return false;
   }
-  n = n/4;
-  return isPowerOfFour(n);
+  return binarysearch(arr,target,0,arr.length-1);
 };
-console.log(isPowerOfFour(6));
+console.log(
+  searchMatrix(
+    [
+      [1, 3, 5, 7],
+      [10, 11, 16, 20],
+      [23, 30, 34, 60],
+    ],
+    20
+  )
+);
