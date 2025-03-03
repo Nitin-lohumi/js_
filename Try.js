832d3fdba0b8b60f6b0149eac6cf9eff7550cb40
Nitin-Lohumi <lohuminitin@gmail.com>
2025-03-03 10:16:21 +0530

    add

--- Try.js
+++ Try.js
@@ -1,27 +1,20 @@
-var setZeroes = function (matrix) {
-  let map = new Map();
-  let m = matrix.length;
-  let n = matrix[0].length;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (matrix[i][j] == 0) {
-        map.set(`${i},${j}`, matrix[i][j]);
-      }
+var pivotArray = function (nums, pivot) {
+  let res = [];
+  for (let i = 0; i < nums.length; i++) {
+     if(nums[i]<pivot){
+        res.push(nums[i]);
+     }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i]==pivot){
+        res.push(pivot);
     }
   }
-  for (const [key, _] of map.entries()) {
-    const Indexes = key.split(",");
-    let index_i = Number(Indexes[0]);
-    let index_j = Number(Indexes[1]);
-    for (let i = 0; i < m; i++) {
-      matrix[i][index_j] = 0;
-    }
-    for (let i = 0; i < n; i++) {
-      matrix[index_i][i] = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i]>pivot){
+        res.push(nums[i]);
     }
   }
-  return matrix;
+  return res;
 };
-console.log(
-  setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]])
-);
+console.log(pivotArray([9, 12, 5, 10, 14, 3, 10], 10));
